80758a9d9f1d08fa666e6ae5137781876187b8c7
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2021-03-08 22:33:48 -0500

    deploy

---
+++ get_data.py
@@ -0,0 +1,34 @@
+from urllib.request import urlopen as uRequest 
+from bs4 import BeautifulSoup as soup
+
+def get_data():
+    myUrl = 'https://weather.gc.ca/city/pages/on-137_metric_e.html' # Environment Canada URL
+    uClient = uRequest(myUrl)
+    page_html = uClient.read() # Reads the HTML source from the URL and puts it to a variable
+    uClient.close()
+
+    # parses html code into a variable
+    page_soup = soup(page_html, "html.parser") # Actually parses the HTML elements using BeautifulSoup
+    container = page_soup.find("div",{"id":"container"}) # Finds container div (where all the data is stored(inspect element of find out more))
+    return container
+
+def get_temp(container):
+    temp = container.find("span",{"class":"wxo-metric-hide"}).text # Gets temp data from appropiate element
+    return temp
+
+def get_condition(container):
+    condition = container.findAll("dd",{"class":"mrgn-bttm-0"}) # Gets weather condition from appropitate element
+    return condition[2].text
+
+def get_date_of_data(container):
+    pass # W.I.P. (find the appropiate element for the date somewhere on the site later)
+
+
+
+    
+
+
+
+
+
+
---
+++ main.py
@@ -0,0 +1,35 @@
+import discord
+from get_data import get_temp, get_condition, get_data
+from discord.ext import commands
+
+client = commands.Bot(command_prefix = 'w/') # Defines the command prefix to be "w/"
+
+@client.event
+async def on_ready(): # When the bot and the Discord API are ready, print this command...
+    print('Bot is on!')
+
+@client.command()
+async def ping(ctx): # ctx = context, the function name IS THE context. So in this function: "ping" IS the context. So, the user must do "w/ping" to activate this command. Same applies for the rest.
+    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+
+@client.command()
+async def hello(ctx):
+    await ctx.send('***Hi there! I am WeatherBot.***')
+
+@client.command()
+@commands.is_owner()
+async def shutdown(ctx): # Shutdown command for the bot to logout of discord
+    await ctx.send("Shutting down...")
+    await ctx.bot.logout()
+
+@client.command()
+async def temp(ctx): # "w/temp"
+    container = get_data() # Calls the get_data function in the get_data file
+    temp = get_temp(container) # Calls to get the temperature data from the get_data file
+    condition = get_condition(container) # Calls to get weather condition
+    if condition == "Mostly Cloudy":
+        await ctx.send("The temperature is " + temp + " in London, Ontario. Condition: " + condition + " with a chance of meatballs") # Just for movie fun!
+    else:
+        await ctx.send("The temperature is " + temp + " in London, Ontario. Condition: " + condition)
+
+client.run('ODE4NTc5MjY2MzA4MDE0MTQw.YEaHWQ.g_6CyUqjlVG9R-EDCdTyygx9h3w') # Token (if re-gen, plz replace it.)
