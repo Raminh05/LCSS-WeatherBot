c4c24705915516dcfe803fb43aa604d042ec1ecb
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2021-03-12 13:17:39 -0500

    Update (March, 12th)
    - Now has conditional weather icons! (e.g. if sunny, then display sun gif)
    - Now changes voice channel names to display temperature readily

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import discord
 from get_data import get_temp, get_condition, get_data
 from discord.ext import commands
 import os
+from datetime import datetime
 
 client = commands.Bot(command_prefix = 'w/', description="") # Defines the command prefix to be "w/"
 
@@ -22,17 +23,6 @@ async def ping(ctx): # ctx = context, the function name IS THE context. So in th
 @client.command()
 async def hello(ctx):
     await ctx.send('***Hi there! I am WeatherBot.***')
- 
-@client.command()
-async def embed(ctx): # W.I.P. This is only for testing the embed functionality. 
-    container = get_data() 
-    temp = get_temp(container)
-    condition = get_condition(container)
-    embedVar = discord.Embed(title="Weather for London, ON", description="Source: Environment Canada", color=0x00ff00)
-    embedVar.add_field(name="Temperature", value=temp, inline=False)
-    embedVar.set_image(url="https://weather.gc.ca/weathericons/30.gif")
-    embedVar.add_field(name="Condition", value=condition, inline=False)
-    await ctx.send(embed=embedVar)
 
 @client.command()
 @commands.is_owner()
@@ -40,15 +30,51 @@ async def shutdown(ctx): # Shutdown command for the bot to logout of discord
     await ctx.send("Shutting down...")
     await ctx.bot.logout()
 
+def get_weather_icon(condition): # New Feature (03/12)
+    if condition == "Sunny" or "Clear":
+        image_url = "https://weather.gc.ca/weathericons/00.gif"
+        return image_url
+    elif condition == "Mainly Sunny" or "A mix of sun and cloud" or "A few clouds" or "Partly Cloudy":
+        image_url = "https://weather.gc.ca/weathericons/01.gif"
+        return image_url
+    elif condition == "Mainly Cloudy":
+        image_url = "https://weather.gc.ca/weathericons//03.gif"
+        return image_url
+    elif condition == "Cloudy":
+        image_url = "https://weather.gc.ca/weathericons/10.gif"
+        return image_url
+    else:
+        image_url = "https://lh3.googleusercontent.com/proxy/dwmrzvnXgrGRbDuGIK_ZMU5sseh0bAkqg1_C3lH0Pfo2YBUrnfjrVXH5SerRs6rpSM52WVIEMfPGE6nVnR4BMM0Kk9_flPfd0esyPwxKZnoU3hhCHfrkCmMCTPSqiiAwc1-CVYdzYwiy"
+        return image_url
+
 @client.command()
 async def temp(ctx): # "w/temp"
+    # Gets all the variables needed
+    now = datetime.now() # Gets time
+    current_time = now.strftime("%H:%M") # Parses time to only be in hours and minutes
     container = get_data() # Calls the get_data function in the get_data file
     temp = get_temp(container) # Calls to get the temperature data from the get_data file
     condition = get_condition(container) # Calls to get weather condition
 
-    if condition == "Mostly Cloudy":
-        await ctx.send("The temperature is " + temp + " in London, Ontario. Condition: " + condition + " with a chance of meatballs") # Just for movie fun!
-    else:
-        await ctx.send("The temperature is " + temp + " in London, Ontario. Condition: " + condition)
+    # Changes channel name to display temperature stats readily (new feature: 03/12)
+    channel = client.get_channel(819952301389119489)
+    await channel.edit(name="Temperature: " + temp)
+
+    image_url = get_weather_icon(condition)
+
+    embedVar = discord.Embed(title="Weather for London, ON", description="Source: Environment Canada", color=0x0000ff) # Moved from test embed function
+    
+    embedVar.add_field(name="Temperature", value=temp, inline=False)
+    embedVar.set_image(url=image_url)
+    embedVar.add_field(name="Condition", value=condition, inline=False)
+    embedVar.set_footer(text="Data retreived at: " + current_time)
+
+    await ctx.send(embed=embedVar)
 
 client.run(api_key)
+
+# Uncomment and re-implement to def temp if need be.
+ # if condition == "Mostly Cloudy":
+        #await ctx.send("The temperature is " + temp + " in London, Ontario. Condition: " + condition + " with a chance of meatballs") # Just for movie fun!
+    #else:
+        #await ctx.send("The temperature is " + temp + " in London, Ontario. Condition: " + condition)
