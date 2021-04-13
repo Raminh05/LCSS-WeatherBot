c8e6617e1e935662e15841ce78c1e3479f302bd9
Raminh05 <77513059+Raminh05@users.noreply.github.com>
2021-04-12 22:42:12 -0400

    Feature update (04/12)
    What's new?
    - Cogs implementation (for non-weather commands anyhow)
    - Country code arguments now optional
    - London data now includes high and low temperatures for the day (courtesy of Environment Canada)
    - Code clean-up and restructuring

--- get_data.py
+++ get_data.py
@@ -1,19 +1,50 @@
 import requests, json
+from bs4 import BeautifulSoup as soup
+from datetime import datetime
+from urllib.request import urlopen as uRequest 
+import discord
 
-api_key = ""
-base_url = "http://api.openweathermap.org/data/2.5/weather?"
+api_key = "b4f6dd2094bdd5048ce9025a901553df"
+api_key_geo = "pk.eyJ1IjoiY2Fubm9saSIsImEiOiJja21udzZpN3AxeXJmMm9zN3BuZGR3aTE0In0.w62dorEJ-QKwtJSswhRVaQ"
+base_url_city = "http://api.openweathermap.org/data/2.5/weather?"
+base_url_geocode = "https://api.mapbox.com/geocoding/v5/mapbox.places/"
 
-def get_city(city):
-    city_name = city
-    complete_url = base_url + "q=" + city_name + "&units=metric" "&appid=" + api_key
-    return complete_url
+def get_cords(city_name, country):
+    complete_url_geo = base_url_geocode + city_name + ".json?country=" + country + "&access_token=" + api_key_geo
+    return complete_url_geo
 
-def get_response(complete_url):
-    response = requests.get(complete_url) # API call
+def get_response_city(complete_url_city):
+    response = requests.get(complete_url_city) # API call
     x = response.json() # Converts json to python-understanable format stuff
     return x
 
-def parse_response(x):
+def get_response_geo(complete_url_geo):
+    response = requests.get(complete_url_geo)
+    x_geo = response.json()
+    main_geo_info = x_geo["features"]
+    main_cords = main_geo_info[0]["center"]
+    longitude = main_cords[0]
+    latitude = main_cords[1]
+    complete_weather_geo_url = "http://api.openweathermap.org/data/2.5/weather?" + "lat=" + str(latitude) + "&lon=" + str(longitude) + "&units=metric" "&appid=" + api_key
+    response_geocoded = requests.get(complete_weather_geo_url)
+    geo_weather_data = response_geocoded.json()
+    return geo_weather_data
+
+def parse_response_geo(response_geocoded):
+    y = response_geocoded["main"]
+    z = response_geocoded["weather"]
+    w = response_geocoded["sys"]
+    cords = response_geocoded["coord"]
+
+    country_code = w["country"]
+    condition = z[0]["description"]
+    icon = z[0]["icon"]
+    temp = str(y["temp"]) + chr(176) + "C"
+    full_icon_url = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
+    
+    return condition, temp, full_icon_url, country_code, cords
+    
+def parse_response_city(x):
     if x["cod"] != "404":
         y = x["main"]
         z = x["weather"]
@@ -21,13 +52,108 @@ def parse_response(x):
         condition = z[0]["description"]
         icon = z[0]["icon"]
         temp = str(y["temp"]) + chr(176) + "C"
+        temp_low = str(y["temp_min"]) + chr(176) + "C"
+        temp_high = str(y["temp_max"]) + chr(176) + "C"
 
         full_icon_url = "http://openweathermap.org/img/wn/" + icon + "@2x.png"
 
+        return condition, temp, full_icon_url, temp_low, temp_high
+
     else:
-        print("City not found")
+        print("City not found...Falling back to cords.")
+
+def get_high_low_london():
+    data_url = "https://weather.gc.ca/city/pages/on-137_metric_e.html"
+    uClient = uRequest(data_url)
+    page_html = uClient.read()
     
-    return condition, temp, full_icon_url
+    page_soup = soup(page_html, "html.parser") # Actually parses the HTML elements using BeautifulSoup
+    container = page_soup.find("div",{"id":"container"}) # Finds container div (where all the data is stored(inspect element of find out more))
+
+    high_temp = container.find("span",{"class":"high wxo-metric-hide"}).text
+    low_temp = container.find("span",{"class":"low wxo-metric-hide"}).text
+    
+    return low_temp, high_temp
+
+
+def embed_london():
+    now = datetime.now() # Gets time
+    current_time = now.strftime("%H:%M") # Parses time to only be in hours and minutes
+    x = get_response_city("http://api.openweathermap.org/data/2.5/weather?q=London,%20CA&units=metric&appid=b4f6dd2094bdd5048ce9025a901553df") # API call
+    data = parse_response_city(x) # Parses json data from API call
+
+    
+    condition = data[0] # Gets weather condition from json data
+    temp = data[1] # Gets temperature from json data
+    image_url = data[2] # calls get_icon to get weather icon according to the condition (Update 03/18)
+    temp_low = get_high_low_london()[0]
+    temp_high = get_high_low_london()[1]
+
+    embedVar = discord.Embed(title="Weather for London, Ontario", description="Source: OpenWeatherMap/Environment Canada", color=0x0000ff) # Moved from test embed function
+    embedVar.add_field(name="Temperature", value=temp, inline=False)
+    embedVar.set_image(url=image_url)
+    embedVar.add_field(name="Condition", value=condition, inline=False)
+    embedVar.add_field(name="High for Today:", value=temp_high, inline=False)
+    embedVar.add_field(name="Low for Today:", value=temp_low, inline=False)
+    embedVar.set_footer(text="Data retreived at: " + current_time + " London, ON time.")
+
+    return embedVar, temp 
+
+def embed_everywhere_else(city_arg, country_arg):
+    # Gets all the variables needed
+    if country_arg == "":
+        now = datetime.now()
+        current_time = now.strftime("%H:%M")
+        x = get_response_geo("https://api.mapbox.com/geocoding/v5/mapbox.places/" + city_arg + ".json?access_token=" + "pk.eyJ1IjoiY2Fubm9saSIsImEiOiJja21udzZpN3AxeXJmMm9zN3BuZGR3aTE0In0.w62dorEJ-QKwtJSswhRVaQ")
+        print(x)
+        data = parse_response_geo(x)
+    else:
+        now = datetime.now() # Gets time
+        current_time = now.strftime("%H:%M") # Parses time to only be in hours and minutes
+        complete_url = get_cords(city_arg, country_arg) # Calls the get_city and fills it in with the command arguement from the user
+        x = get_response_geo(complete_url) # API call
+        print(x)
+        data = parse_response_geo(x) # Parses json data from API call
+       
+    condition = data[0] # Gets weather condition from json data
+    temp = data[1] # Gets temperature from json data
+    image_url = data[2] # calls get_icon to get weather icon according to the condition (Update 03/18)
+    country_code = data[3]
+    cords = data[4]
+    
+    embedVar = discord.Embed(title="Weather for " + city_arg + ", " + country_code, description="Source: OpenWeatherMap", color=0x0000ff) # Moved from test embed function   
+    embedVar.add_field(name="Temperature", value=temp, inline=False)
+    embedVar.set_image(url=image_url)
+    embedVar.add_field(name="Condition", value=condition, inline=False)
+    embedVar.add_field(name="Coordinates:", value=cords, inline=False)
+    embedVar.set_footer(text="Data retreived at: " + current_time + " London, ON local time.")
+
+    return embedVar
+
+get_high_low_london()
+
+
+
+
+
+
+
+# For debugging
+#response_geocoded = get_response_geo("https://api.mapbox.com/geocoding/v5/mapbox.places/vancouver,%20CA.json?access_token=pk.eyJ1IjoiY2Fubm9saSIsImEiOiJja21udzZpN3AxeXJmMm9zN3BuZGR3aTE0In0.w62dorEJ-QKwtJSswhRVaQ")
+#data = parse_response_geo(response_geocoded)
+#print(data)
+
+
+
+
+
+        
+    
+    
+
+
+    
+    
 
 
 
--- main.py
+++ main.py
@@ -1,9 +1,7 @@
 import discord
-from get_data import get_city, get_response, parse_response
+from get_data import embed_london, embed_everywhere_else
 from discord.ext import commands
 import os
-from datetime import datetime
-import time
 
 client = commands.Bot(command_prefix = 'w/', description="") # Defines the command prefix to be "w/"
 
@@ -12,83 +10,50 @@ with open(".env", encoding="utf-8") as f:
     api_key = f.read()
     f.close()
 
-@client.event
-async def on_ready(): # When the bot and the Discord API are ready, print this command...
-    await client.change_presence(activity=discord.Game(name="with the weather"))
-    print('Bot is on!')
+@client.command()
+async def load(ctx, extension):
+    client.load_extension(f'cogs.{extension}')
+    await ctx.send("Kelcogg's ")
+
+@client.command()
+async def unload(ctx, extension):
+    client.unload_extension(f'cogs.{extension}')
+
+# w/temp. embeds weather data.
+@client.command()
+async def temp(ctx, city_arg, country_arg=""): 
+    if country_arg == "":
+        await ctx.send("No country argument! The location may be incorrect as a result.")
     
-@client.command()
-async def ping(ctx): # ctx = context, the function name IS THE context. So in this function: "ping" IS the context. So, the user must do "w/ping" to activate this command. Same applies for the rest.
-    await ctx.send(f'Pong! {round(client.latency * 1000)}ms')
+    embedVar = embed_everywhere_else(city_arg, country_arg) # calls function from get_data
 
-@client.command()
-async def hello(ctx):
-    await ctx.send('***Hi there! I am WeatherBot.***')
-
-@client.command()
-@commands.is_owner()
-async def shutdown(ctx): # Shutdown command for the bot to logout of discord
-    await ctx.send("Shutting down...")
-    await ctx.bot.logout()
-
-@client.command()
-async def temp(ctx, city_arg): # "w/temp"
-    # Gets all the variables needed
-    now = datetime.now() # Gets time
-    current_time = now.strftime("%H:%M") # Parses time to only be in hours and minutes
-    complete_url = get_city(city_arg) # Calls the get_city and fills it in with the command arguement from the user
-    x = get_response(complete_url) # API call
-    data = parse_response(x) # Parses json data from API call
-
-    condition = data[0] # Gets weather condition from json data
-    temp = data[1] # Gets temperature from json data
-
-    if city_arg == "London, CA":     # Changes channel name to display temperature stats readily only if requested city is London, ON (new feature: 03/12) 
-        channel = client.get_channel(822198652398600242)
-        await channel.edit(name="Temperature: " + temp)
-
-    image_url = data[2] # calls get_icon to get weather icon according to the condition (Update 03/18)
-
-    embedVar = discord.Embed(title="Weather for " + city_arg, description="Source: OpenWeatherMap", color=0x0000ff) # Moved from test embed function
-    
-    embedVar.add_field(name="Temperature", value=temp, inline=False)
-    embedVar.set_image(url=image_url)
-    embedVar.add_field(name="Condition", value=condition, inline=False)
-    embedVar.set_footer(text="Data retreived at: " + current_time)
-
-    await ctx.send(embed=embedVar)
+    await ctx.send(embed=embedVar) # sends embed
+    await ctx.send("Another day another ~~dollar~~ weather request!")
     print("Sucessful!")
 
+# Error for missing city argument.
 @temp.error
 async def temp_error(ctx, error):
     if isinstance(error, commands.MissingRequiredArgument):
-        await ctx.send("Missing arguement: Please type in the City Name! Defaulting to London, Ontario...")
+        await ctx.send("Nice try Buster...(sorry) but you need the city name if you don't want the weather for London, Ontario!")
 
-        now = datetime.now() # Gets time
-        current_time = now.strftime("%H:%M") # Parses time to only be in hours and minutes
-        x = get_response("http://api.openweathermap.org/data/2.5/weather?q=London,%20CA&units=metric&appid=b4f6dd2094bdd5048ce9025a901553df") # API call
-        data = parse_response(x) # Parses json data from API call
+        data_and_embed = embed_london() 
+        await ctx.send(embed=data_and_embed[0]) # sends embed
 
-        condition = data[0] # Gets weather condition from json data
-        temp = data[1] # Gets temperature from json data
-
-           # Changes channel name to display temperature stats readily only if requested city is London, ON (new feature: 03/12) 
+        # Changes channel name to display temperature stats readily only if requested city is London, ON (new feature: 03/12) 
         channel = client.get_channel(822198652398600242)
-        await channel.edit(name="Temperature: " + temp)
+        await channel.edit(name="Temperature: " + data_and_embed[1])
 
-        image_url = data[2] # calls get_icon to get weather icon according to the condition (Update 03/18)
-
-        embedVar = discord.Embed(title="Weather for London, Ontario", description="Source: OpenWeatherMap", color=0x0000ff) # Moved from test embed function
         
-        embedVar.add_field(name="Temperature", value=temp, inline=False)
-        embedVar.set_image(url=image_url)
-        embedVar.add_field(name="Condition", value=condition, inline=False)
-        embedVar.set_footer(text="Data retreived at: " + current_time)
-
-        await ctx.send(embed=embedVar)
         print("Sucessful!")
 
+# Iterates through all filenames in the cogs directory to load all cogs.
+for filename in os.listdir("./cogs"):
+    if filename.endswith(".py"):
+        client.load_extension(f'cogs.{filename[:-3]}')
+
 client.run(api_key)
+
 
 # Uncomment and re-implement to def temp if need be.
  # if condition == "Mostly Cloudy":
